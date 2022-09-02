6ec8c86938af7a97a58a5df10f40eef019ab8572
NaTa <nik088-75@mail.ru>
2022-09-02 20:55:33 +0300

    создана папка

---
+++ Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿{
+    int a,b;
+    Console.WriteLine("Введите числа :");
+    a = Convert.ToInt32(Console.ReadLine());
+    b = Convert.ToInt32(Console.ReadLine());
+    if (a>b)
+    {
+        Console.WriteLine(" наибольшее число, a");
+    }
+    else
+    {
+        Console.WriteLine(" наибольшее число, b");
+    }
+}
